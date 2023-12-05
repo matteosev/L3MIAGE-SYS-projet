3c823593e1a50eff3207ddafa8ffcbe421f6deb3
hounkara <91179744+hounkara@users.noreply.github.com>
2023-12-05 08:51:19 +0100

    Update commons.h

--- headers/commons.h
+++ headers/commons.h
@@ -1,2 +1,38 @@
+typedef enum 
+{
+    VALENCE,
+    GRENOBLE,
+    MONTELIMAR,
+    PARIS
+} City;
+
+typedef struct 
+{
+    int hour;
+    int minute;
+} Time;
+
+typedef struct 
+{
+    int id;
+    City city_from;
+    City city_to;
+    Time time_from;
+    Time time_to;
+    double price;
+    int reduc;
+    int suppl;
+} Train;
+
+typedef struct 
+{
+    City city_from;
+    City city_to;
+    Time time_from_1;
+    Time time_from_2;
+} Request;
+
+
+
 void write_size_then_msg(int socket, void *msg);
 void read_size_then_msg(int socket, void *msg);
