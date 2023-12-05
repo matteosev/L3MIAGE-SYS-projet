0fb5afa7d9939d819d063a34cd85149278b19be3
hounkara <91179744+hounkara@users.noreply.github.com>
2023-12-05 09:24:51 +0100

    yo sir

--- sources/commons.c
+++ sources/commons.c
@@ -13,3 +13,16 @@ void read_size_then_msg(int socket, void *msg) {
     read(socket, &size, sizeof(size));
     read(socket, msg, size);
 }
+
+
+void print_train(Train train){
+    printf("id: %d\n",train.id);
+    printf("city_from_train_cam: %d\n",train.city_from);
+    printf("city_that_train_will_go_to: %d\n",train.city_to);
+    printf("Time_From: %02d:%02d\n", train.time_from.hour, train.time_from.minute);
+    printf("Time_To: %02d:%02d\n", train.time_to.hour, train.time_to.minute);
+    printf("the_price_is: %lf\n",train.price);
+    prinf("the_reduc: %d\n",train.reduc);
+    printf("the_supp: %d\n",train.suppl);
+
+}
