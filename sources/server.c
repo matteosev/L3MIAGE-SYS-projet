19ca3ff8b7282b644a5a4b9dea6ed9141bb91c56
Porcu Lorenzo <porcul@im2ag-203-13.u-ga.fr>
2023-12-05 13:23:11 +0100

    qefgze

--- sources/server.c
+++ sources/server.c
@@ -24,12 +24,55 @@ int main(int argc, char **argv) {
     bind(sock_listen, (const struct sockaddr *)&sockaddr, size_addr);
     listen(sock_listen, 0);
 
+    const int nb_train = 3;
+    Train trains[nb_train];
+    
+    for(int i = 0; i < nb_train; i++){
+
+        trains[i].id = i;
+        switch (i)
+        {
+            case 0 :
+
+                strcpy(trains[i].city_from, "MONTELIMAR");
+                strcpy(trains[i].city_to, "PARIS");
+                break;
+            
+            case 1 :
+
+                strcpy(trains[i].city_from, "GRENOBLE");
+                strcpy(trains[i].city_to, "VALENCE");
+                break;
+            
+            default :
+
+                strcpy(trains[i].city_from, "PARIS");
+                strcpy(trains[i].city_to, "VALENCE");
+                break;
+        
+        }
+        
+        trains[i].time_from.hour = 10;
+        trains[i].time_from.minute = 0;
+        trains[i].time_to.hour = 10;
+        trains[i].time_to.minute = 30;
+        trains[i].price = 9.80;
+        trains[i].reduc = 0;
+        trains[i].suppl = 0;
+    } 
+
+
+    
+
     while(1) {
+
+        Train *filtered_trains;
 
         int sock_service = accept(sock_listen, (struct sockaddr *)&sockaddr, &size_addr);
         
         int pid = fork();
         switch(pid) {
+
             case -1:
                 perror("Erreur");
                 break;
@@ -43,33 +86,14 @@ int main(int argc, char **argv) {
                 do{
                     
                     read(sock_service, &req, sizeof(req));
-            
-                    int nb_train = 2;
 
-                    Train monTrain;
-                    Time horaire1;
-                    Time horaire2;
-
-                    horaire1.hour = 10;
-                    horaire1.minute = 30;
-
-                    horaire2.hour = 11;
-                    horaire2.minute = 30;
-
-                    monTrain.id = 10;
-                    strcpy(monTrain.city_from, "MONTELIMAR");
-                    strcpy(monTrain.city_to, "PARIS");
-                    monTrain.time_from = horaire1;
-                    monTrain.time_to = horaire2;
-                    monTrain.price = 9.80;
-                    monTrain.reduc = 0;
-                    monTrain.suppl = 0;
-
-                    write(sock_service, &nb_train, sizeof(nb_train));
+                    int nb_train_filtered = filter_train_from_array(trains, nb_train, filtered_trains, req.city_from, req.city_to, req.time_from_1, req.time_from_2);
+                    printf("%d\n", nb_train_filtered);
+                    write(sock_service, &nb_train_filtered, sizeof(nb_train_filtered));
                     
-                    for(int i = 0; i < nb_train; i++){
+                    for(int i = 0; i < nb_train_filtered; i++){
 
-                        write(sock_service, &monTrain, sizeof(monTrain));
+                        write(sock_service, &filtered_trains[i], sizeof(filtered_trains[i]));
                             
                     }
 
@@ -95,10 +119,9 @@ int main(int argc, char **argv) {
 int filter_train_from_array(Train trains[], int nb_train, Train *trains_filtered, char* city_from, char* city_to, Time time_from, Time time_to){
 
     trains_filtered = malloc(0);
-    int nb_train_filtered;
+    int nb_train_filtered = 0;
 
     for(int i = 0; i < nb_train; i++){
-
         if(check_filter(trains[i], city_from, city_to, time_from, time_to) == 1){
             
             trains_filtered = realloc(trains_filtered, sizeof(trains_filtered) + sizeof(Train));
