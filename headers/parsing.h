98cde97b76383a5d525a73fdeaf324b6788e4945
hounkara <91179744+hounkara@users.noreply.github.com>
2023-12-07 10:14:46 +0100

    Update parsing.h

--- headers/parsing.h
+++ headers/parsing.h
@@ -1,3 +1,6 @@
+#define MAX_CITIES 250
+#define MAX_CITY_NAME_LENGTH 100
+
 int read_trains_from_file(char *filename, Train *trains, int maxTrains, char cities[MAX_CITIES][MAX_CITY_NAME_LENGTH], int *city_count);
 int count_trains(char *filename);
 void add_city_if_new(char cities[MAX_CITIES][MAX_CITY_NAME_LENGTH], int *city_count, const char *city);
